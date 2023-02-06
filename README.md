5a88f7efe26fa60dfa8dcd0989278e05c92018d7
Papp Zoltán <papp.zoltan@infokristaly.hu>
2023-02-06 09:42:55 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# WebDemoProject
+This is a Ract Native demo project for Web and Stack Navigation
+## Install
+yarn install
+
+## Run
+yarn web
+yarn android
---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import HomeScreen from './screens/HomeScreen';
+import AdminScreen from './screens/AdminScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { RootStackParamList } from './screens/RootStackParamList';
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen}
+        />
+        <Stack.Screen name="Admin" component={AdminScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>);
+};
+
+export default App;
